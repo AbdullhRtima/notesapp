dd25e84db4c464b9ccdea9c21811385d859e9473
AbdullhRtima <abdullahaburtima@gmail.com>
2020-02-04 12:03:49 +0200

    heroku fix hostname error

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 const app = require('./app');
 const hostname = app.get('host');
 const port = app.get('port');
-const server = app.listen(port, hostname);
+const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
